39a2cc0a976902c508d5b06fca94b1d0af1074be
loolipin21 <lipin.loo@axium-industries.com>
2026-03-03 11:38:29 +0800

    Rename project from OpenExcel to ExcelOS

--- src/taskpane/index.tsx
+++ src/taskpane/index.tsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import App from "./components/app";
 import "./index.css";
 
-const title = "OpenExcel";
+const title = "ExcelOS";
 
 const rootElement: HTMLElement | null = document.getElementById("container");
 const root = rootElement ? createRoot(rootElement) : undefined;
